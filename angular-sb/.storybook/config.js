ca33e358a0314b090e4e13ccd135f7171e7c4871
Aleksey Pastuhov <aleksey.pastuhov@gmail.com>
2019-04-19 01:30:06 +0300

    Provide jQuery example

--- angular-sb/.storybook/config.js
+++ angular-sb/.storybook/config.js
@@ -1,4 +1,71 @@
-import { configure } from '@storybook/angular';
+import { configure, addParameters } from '@storybook/angular';
+import { themes } from '@storybook/theming';
+
+// Option defaults:
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: true,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: 'right',
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    // theme: themes.dark,
+    theme: undefined,
+  },
+});
 
 // automatically import all files ending in *.stories.ts
 const req = require.context('../src', true, /\.stories\.ts$/);
