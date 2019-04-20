2740b6f03cd29ea9d22e863bbb28d681ffd4a19c
Aleksey Pastuhov <aleksey.pastuhov@gmail.com>
2019-04-20 09:02:31 +0300

    Add addons

--- vue-sb/config/storybook/config.js
+++ vue-sb/config/storybook/config.js
@@ -1,5 +1,79 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@storybook/vue'
+import { configure, addParameters, addDecorator } from '@storybook/vue'
+import { themes } from '@storybook/theming';
+import { withA11y } from '@storybook/addon-a11y';
+
+// Option defaults:
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: true,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: 'bottom',
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    theme: themes.white,
+    // theme: undefined,
+  },
+  backgrounds: [
+    { name: 'white', value: '#eee', default: true },
+    { name: 'black', value: '#333' },
+  ],
+});
+
+addDecorator(withA11y);
 
 const req = require.context('../../src/stories', true, /.stories.js$/)
 
